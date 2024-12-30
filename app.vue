df6a2e1bdba6f391e6168d3477ebb33ea8e5c9be
JCX2927917567 <jiachao.xiong@dragonsoftbravo.com>
2024-12-30 17:40:54 +0800

    feat: 增加loading及进度条

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
+  <!-- 首页加载全屏动画 -->
+  <FullLoading v-if="isFullLoading" />
   <NuxtLayout>
+    <!-- 在页面导航之间显示一个进度条 -->
+    <NuxtLoadingIndicator />
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+// 是否首次加载
+const isFullLoading = ref(true);
+nuxtApp.hook("page:start", () => {
+  isFullLoading.value = true;
+});
+nuxtApp.hook("page:finish", () => {
+  isFullLoading.value = false;
+});
+</script>
