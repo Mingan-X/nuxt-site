e054804d4e1fd2ab41631b84db14799d32d15a03
Jasper <2927917567@qq.com>
2025-03-20 10:32:44 +0800

    更改抓取设置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   },
   nitro: {
     prerender: {
-      crawlLinks: false, // 禁用爬虫抓取，可以逐一定位问题
+      // crawlLinks: false, // 禁用爬虫抓取，可以逐一定位问题
     },
   },
   app: {
