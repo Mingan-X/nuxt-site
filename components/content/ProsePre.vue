74f64d3b627f7bd3bca8f3c5c50308eda8a7316e
Jasper <2927917567@qq.com>
2025-04-02 17:15:17 +0800

    feat: 完善自定义ProsePre组件

--- components/content/ProsePre.vue
+++ components/content/ProsePre.vue
@@ -1,20 +1,40 @@
 <template>
   <div
     id="custom-pose-pre"
-    class="flex flex-col relative .dark:bg-#020618 .dark:border-#314158 .light:bg-#fff border border-oklch(.929 .013 255.508) rounded-t-6px px-4"
+    class="flex flex-wrap relative .dark:bg-#020618 .dark:border-#314158 .light:bg-#fff border border-#e2e8f0 rounded-t-6px px-4"
   >
-    <div>
-      <span>{{ language }}</span>
+    <div class="w-full flex justify-between">
+      <div class="flex items-center h-28px">
+        <n-icon
+          size="12"
+          class="invert-80"
+          @click="changeExpand"
+          :component="expandStatus ? CaretDown : CaretForward"
+        ></n-icon>
+        <span class="text-12px ml-12px">{{ language }}</span>
+      </div>
+      <button
+        @click="copyToClipboard"
+        class="w-24px h-24px border rounded .dark:bg-#020618 .dark:border-#314158 bg-transparent hover:bg-#F0F0F0 .dark:hover:bg-#0f172b"
+      >
+        <n-icon><CopyOutline /></n-icon>
+      </button>
     </div>
   </div>
   <pre
-    :class="$props.class"
+    :class="expandStatus ? $props.class : 'h-0 m-0! b-b-none'"
     class="border border-t-none .dark:border-#314158!"
   ><slot /></pre>
 </template>
 
 <script setup lang="ts">
-import { CaretDown, CaretForward } from "@vicons/ionicons5";
+import { CaretDown, CaretForward, CopyOutline } from "@vicons/ionicons5";
+// @ts-ignore
+import { useClipboard } from "@vueuse/core";
+import { useMessage } from "naive-ui";
+const message = useMessage();
+const { copy, copied, isSupported } = useClipboard({ legacy: true });
+const expandStatus = ref(true);
 const props = defineProps({
   code: {
     type: String,
@@ -41,7 +61,17 @@ const props = defineProps({
     default: null,
   },
 });
-console.log(props);
+const changeExpand = () => {
+  expandStatus.value = !expandStatus.value;
+};
+const copyToClipboard = async () => {
+  await copy(props.code);
+  if (!isSupported.value) {
+    message.warning("您的浏览器不支持");
+  } else if (copied.value) {
+    message.success("复制成功");
+  }
+};
 </script>
 
 <style lang="less" scoped>
